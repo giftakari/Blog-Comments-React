a17f4e805abcea61768dca393fa80ebf591c374b
Godsgift Akari <giftakari@yahoo.com>
2018-12-10 17:10:33 +1100

    Complete Project

--- blog-comments/src/index.js
+++ blog-comments/src/index.js
@@ -7,15 +7,22 @@ import ApprovalCard from './ApprocardCard';
 const App = () =>{
 return (
 <div className="ui container comments" >
-<ApprovalCard/>
- <CommentDetails author ="Sam" timeOfPost="Today at 8:56pm"
+ <ApprovalCard>
+  <CommentDetails author ="Sam" timeOfPost="Today at 8:56pm"
   post ="Its not easy but not impossible" avatar ={faker.image.avatar()}
  />
- <CommentDetails author = "Alexander Isa" timeOfPost="Yesterday at 5:56pm" post avatar ={faker.image.avatar()}
- />
+ </ApprovalCard>
+
+ <ApprovalCard>
+ 
+<CommentDetails author = "Alexander Isa" timeOfPost="Yesterday at 5:56pm" post avatar ={faker.image.avatar()} /> 
+</ApprovalCard>
+
+<ApprovalCard>
  <CommentDetails author = "Jenifer Appiah" timeOfPost ="Today at 5:50am"
    post ="Lets build the bond ,lets grow together" avatar ={faker.image.avatar()}
  />
+ </ApprovalCard>
 
 </div>
 
